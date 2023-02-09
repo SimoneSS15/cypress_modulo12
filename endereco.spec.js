d2e96dcb1f551aaf0b5618223f49ca5c8ff452a0
Simone Dos Santos Silva <110856273+SimoneSS15@users.noreply.github.com>
2023-02-09 17:17:58 -0300

    Add files via upload
    Usando massa de dados

--- endereco.spec.js
+++ endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress"/>
+import enderecoPage from '../support/page-objects/endereco.page';
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(()=>{
@@ -10,6 +12,24 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        //Cadastro de endereço
+        enderecoPage.editarenderecoFaturamento('Simone', 'Santos', 'Google', 'Carajas', '15', 'Itapevi', 'São Paulo', '06656350', '11973571191', 'wirus1@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso') //assert
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        enderecoPage.editarenderecoFaturamento(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empresa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].endereco,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].cep,
+            dadosEndereco[0].telefone,
+            dadosEndereco[0].email
+
+            )//vai pegar os dados do array 1
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso') //assert
     });
 });
